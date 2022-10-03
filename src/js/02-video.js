00da035d3a0553cd994c711c9f6e8bf99b92312a
Nikita Mor <nikita.melnuk425@gmail.com>
2022-10-03 21:03:30 +0300

    up

---
+++ src/js/02-video.js
@@ -0,0 +1,53 @@
+import throttle from 'lodash.throttle';
+
+import Player from '@vimeo/player';
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const STORAGE_KEY = 'videoplayer-current-time';
+
+const saveItemStorage = (key, value) => {
+  try {
+    const itemJSON = JSON.stringify(value);
+    localStorage.setItem(key, itemJSON);
+  } catch (error) {
+    console.error('Set state error: ', error.message);
+  }
+};
+
+const onPlay = currentTime => {
+  saveItemStorage(STORAGE_KEY, currentTime.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const readItemStorage = key => {
+  try {
+    const itemJSON = localStorage.getItem(key);
+
+    return itemJSON === itemJSON ? JSON.parse(itemJSON) : null;
+  } catch (error) {
+    console.error('Get state error: ', error.message);
+  }
+};
+
+const savedCurrentTime = readItemStorage(STORAGE_KEY);
+
+const handleReload = () => {
+  player
+    .setCurrentTime(savedCurrentTime)
+    .then(function (seconds) {})
+    .catch(function (error) {
+      switch (error.name) {
+        case 'RangeError':
+          // the time was less than 0 or greater than the video’s duration
+          break;
+
+        default:
+          // some other error occurred
+          break;
+      }
+    });
+};
+
+document.addEventListener('DOMContentLoaded', handleReload);
---
+++ src/js/03-feedback.js
@@ -0,0 +1,71 @@
+import throttle from 'lodash.throttle';
+
+const STORAGE_KEY = 'feedback-form-state';
+
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('submit', handleSubmitForm);
+form.addEventListener('input', throttle(handleInput, 500));
+
+function saveItemStorage(key, value) {
+  try {
+    const itemJSON = JSON.stringify(value);
+    localStorage.setItem(key, itemJSON);
+  } catch (error) {
+    console.error('Set state error: ', error.message);
+  }
+}
+
+function readItemStorage(key) {
+  try {
+    const itemJSON = localStorage.getItem(key);
+
+    return itemJSON === itemJSON ? JSON.parse(itemJSON) : null;
+  } catch (error) {
+    console.error('Get state error: ', error.message);
+  }
+}
+
+function handleInput() {
+  const {
+    elements: { email, message },
+  } = form;
+
+  const dataForm = {
+    email: email.value,
+    message: message.value,
+  };
+
+  return saveItemStorage(STORAGE_KEY, dataForm);
+}
+
+function handleSubmitForm(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, message },
+  } = event.currentTarget;
+
+  if (email.value === '' || message.value === '') {
+    return console.log(alert('Please fill all fields!'));
+  }
+
+  console.log(readItemStorage(STORAGE_KEY));
+
+  event.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function populateTextOnForm() {
+  const savedText = readItemStorage(STORAGE_KEY);
+
+  if (savedText) {
+    const {
+      elements: { email, message },
+    } = form;
+    email.value = savedText.email;
+    message.value = savedText.message;
+  }
+}
+
+document.addEventListener('DOMContentLoaded', populateTextOnForm);
